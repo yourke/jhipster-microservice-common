77dc541c52f68933c6314c7d95adb75425888680
yonker <20986198+yourke@users.noreply.github.com>
2020-07-31 14:02:56 +0800

    修改：请求拦截逻辑调整

--- src/main/java/com/jhipster/common/config/RequestOriginConfiguration.java
+++ src/main/java/com/jhipster/common/config/RequestOriginConfiguration.java
@@ -1,8 +1,10 @@
 package com.jhipster.common.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 import com.jhipster.common.filter.RequestOriginFilter;
 
@@ -15,12 +17,15 @@ import com.jhipster.common.filter.RequestOriginFilter;
 @Configuration
 public class RequestOriginConfiguration {
 
+    @Autowired
+    private Environment env;
+
     @Bean
     public FilterRegistrationBean requestOriginFilterRegistration() {
         // 新建过滤器注册类
         FilterRegistrationBean registration = new FilterRegistrationBean();
         // 添加指定的过滤器
-        registration.setFilter(new RequestOriginFilter());
+        registration.setFilter(new RequestOriginFilter(env));
         // 设置过滤器的URL模式（针对模块间内部接口的校验）
         registration.addUrlPatterns("/api/inner/*");
         // 设置过滤器名称
